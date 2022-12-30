d102fb2299d49eeef68ece0f41fbd3bcedd58174
Vanya <user@orp.local>
2022-12-29 19:29:58 -0500

    added proj, harness

---
+++ mayhem/fuzz-reader.py
@@ -0,0 +1,36 @@
+#! /usr/bin/python3
+
+import atheris
+import sys
+import io
+
+
+with atheris.instrument_imports():
+    from reader import make_reader, InvalidFeedURLError
+
+def fuzz_singleInput(input_bytes):
+    fdp = atheris.FuzzedDataProvider(input_bytes)
+    data_string = fdp.ConsumeUnicodeNoSurrogates(sys.maxsize)
+    data_int = fdp.ConsumeInt(sys.maxsize)
+
+    try:
+        feed_url = data_string
+
+        reader = make_reader("db.sqlite")
+
+        def add_and_update_feed():
+            reader.add_feed(feed_url, exist_ok=True)
+            reader.update_feeds()
+
+        add_and_update_feed()
+
+        feed = reader.get_feed(feed_url)
+    except InvalidFeedURLError:
+        pass
+
+def main():
+    atheris.Setup(sys.argv, fuzz_singleInput)
+    atheris.Fuzz()
+
+if __name__ == "__main__":
+    main()
