734c466e9abc114eaa8c622f057f8d77b988f043
Vanya <user@orp.local>
2022-12-30 01:55:19 -0500

    updated harness

--- mayhem/fuzz-reader.py
+++ mayhem/fuzz-reader.py
@@ -16,9 +16,9 @@ def fuzz_singleInput(input_bytes):
 
         reader = make_reader("db.sqlite")
 
-        reader.add_feed(feed_url, exist_ok=True)
+        # reader.add_feed(feed_url, exist_ok=True)
         # reader.update_feeds()
-        # feed = reader.get_feed(feed_url)
+        feed = reader.get_feed(feed_url)
     except InvalidFeedURLError:
         pass
 
